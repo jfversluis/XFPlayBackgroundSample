2afb26ad9f22092ed7fb6e5fe106168e5fa40e4f
Gerald Versluis <gerald@verslu.is>
2021-04-26 10:21:32 +0200

    Initial commit

---
+++ XFPlayBackgroundSample.iOS/MediaElementCustomRenderer.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.CommunityToolkit.UI.Views;
+using XFPlayBackgroundSample.iOS;
+
+[assembly: Xamarin.Forms.ExportRenderer(typeof(MediaElement), typeof(MediaElementCustomRenderer))]
+namespace XFPlayBackgroundSample.iOS
+{
+    public class MediaElementCustomRenderer : MediaElementRenderer
+    {
+        public MediaElementCustomRenderer()
+        {
+            //avPlayerViewController.UpdatesNowPlayingInfoCenter = false;
+        }
+    }
+}
